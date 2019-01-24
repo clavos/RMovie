48977ffee9a1bc4addc7399927ebcd91884d4a4b
thedeadbrother <kunborribo456258@gmail.com>
2019-01-24 18:41:02 +0100

    Add bootstrap lib

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,9 @@
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/global.scss');
 require('../css/test.scss');
+require('..js\media\style.css');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// var $ = require('jquery');
+var $ = require('jquery');
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
